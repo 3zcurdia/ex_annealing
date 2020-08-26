9fef3ac1ab298b1798c3a74ee2ab91ab563f3380
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-08-26 15:01:28 -0500

    Initial mix structure

---
+++ lib/annealing.ex
@@ -0,0 +1,18 @@
+defmodule Annealing do
+  @moduledoc """
+  Documentation for `Annealing`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Annealing.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/annealing_test.exs
@@ -0,0 +1,8 @@
+defmodule AnnealingTest do
+  use ExUnit.Case
+  doctest Annealing
+
+  test "greets the world" do
+    assert Annealing.hello() == :world
+  end
+end
