fff6e04c16d808049a8a9e3e7c29ee2c6816374a
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-08-29 08:29:02 -0500

    Annealing escript

--- lib/annealing.ex
+++ lib/annealing.ex
@@ -2,4 +2,20 @@ defmodule Annealing do
   @moduledoc """
   Documentation for `Annealing`.
   """
+  alias Annealing.{Metal, Simulator}
+
+  def main(_args) do
+    list = [{60, 200}, {180, 200}, {40, 120}, {100, 120}, {20, 40}]
+    metal = %Metal{list: list} |> Metal.energy(&distance/1)
+    IO.inspect(metal)
+    result = Simulator.run(metal, 10_000, 0.01, &distance/1)
+    IO.inspect(result)
+  end
+
+  def distance(list) do
+    list
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(fn [{ax, ay}, {bx, by}] -> :math.pow(ax - bx, 2) + :math.pow(ay - by, 2) end)
+    |> Enum.sum()
+  end
 end
